1289a4d978c6819661a2989e1e0d8470525d4928
Muhammad Mario <acernitro58@gmail.com>
2022-08-14 20:21:01 +0700

    add search bar

--- src/components/organism/Navbar.js
+++ src/components/organism/Navbar.js
@@ -5,12 +5,14 @@ import Cart from "../atoms/Cart";
 import Hamburger from "../atoms/Hamburger";
 import Logo from "../atoms/Logo";
 import Navlink from "../atoms/Navlink";
+import Search from "../atoms/Search";
 
 function Navbar() {
   return (
     <div className="w-full sticky top-0 border-b border-slate-900/10 h-20 flex justify-between items-center px-2 z-10 md:px-10">
       <Hamburger />
       <Logo />
+      <Search />
       <div className="flex gap-4 justify-center items-center">
         <Navlink />
         {/* <ButtonLogin />
