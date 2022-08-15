7c968213d4ac130d9a8bfdde2ef853043f808ab7
Muhammad Mario <acernitro58@gmail.com>
2022-08-15 19:09:23 +0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Card from "./components/atoms/Card";
 import Navbar from "./components/organism/Navbar";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/product/:productId" element={} /> */}
+      </Routes>
     </div>
   );
 }
